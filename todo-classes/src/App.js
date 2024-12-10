25392ca11a9e31ba8fc4d61d40250c45500c0107
xGentuso <ryan.mota@triosstudent.com>
2024-12-10 11:49:12 -0500

    added Todo Classes

---
+++ todo-classes/src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { TodoBanner } from './TodoBanner';
+import { TodoCreator } from './TodoCreator';
+import { TodoRow } from './TodoRow';
+
+export default class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "Adam",
+      todoItems: [{ action: "Buy Flowers", done: false },
+      { action: "Get Shoes", done: false },
+      { action: "Collect Tickets", done: true },
+      { action: "Call Joe", done: false }]
+    }
+  }
+  
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value });
+  }
+  
+  createNewTodo = (task) => {
+    if (!this.state.todoItems
+      .find(item => item.action === this.task)) {
+      this.setState({
+        todoItems: [...this.state.todoItems,
+        { action: this.task, done: false }],
+        newItemText: ""
+      });
+    }
+  }
+  
+  toggleTodo = (todo) => this.setState({
+    todoItems:
+      this.state.todoItems.map(item => item.action === todo.action
+        ? { ...item, done: !item.done } : item)
+  });
+  
+  todoTableRows = () => this.state.todoItems.map(item =>
+    <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo} /> )
+  
+  render = () =>
+    <div>
+      <TodoBanner name={ this.state.userName } tasks={ this.state.todoItems } />
+      <div className="container-fluid">
+        <TodoCreator callback= { this.createNewTodo } />
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr><th>Description</th><th>Done</th></tr>
+          </thead>
+          <tbody>{this.todoTableRows()}</tbody>
+        </table>
+      </div>
+    </div>
+}
